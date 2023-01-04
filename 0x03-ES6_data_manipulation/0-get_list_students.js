92fb6e51d9603dbd9e0508807eeeea3d25127e81
Obumbillions <obumnemeokoy914@gmail.com>
2023-01-04 21:20:12 +0100

    Ch: Added code and structure

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+  return [
+	      { id: 1, firstName: 'Guillaume', loexport default function getListStudents() {
+cation: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
